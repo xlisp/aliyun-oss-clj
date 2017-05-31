521f563bc0fc6186502286dfaf0534cbe390a7cf
chanshunli <chanshunli@gmail.com>
2017-05-31 18:39:46 +0800

    要把这个授权策略 AliyunSTSAssumeRoleAccess 给到授权的那个用户。

--- src/aliyun_oss_clj/core.clj
+++ src/aliyun_oss_clj/core.clj
@@ -1,19 +1,41 @@
 (ns aliyun-oss-clj.core
   (:import
-   [java.io
-    BufferedReader
-    ByteArrayInputStream
-    File
-    InputStream
-    InputStreamReader]
-   [java.util.List]
-   [java.net.URLClassLoader]
-   [com.aliyun.oss
-    ClientException
-    OSSClient
-    OSSException]
-   [com.aliyun.oss.model
-    BucketInfo
-    OSSObject
-    OSSObjectSummary
-    ObjectListing]))
+   [com.aliyuncs DefaultAcsClient]
+   [com.aliyuncs.exceptions.ClientException]
+   [com.aliyuncs.http MethodType ProtocolType]
+   [com.aliyuncs.profile DefaultProfile IClientProfile]
+   [com.aliyuncs.sts.model.v20150401 AssumeRoleRequest AssumeRoleResponse]))
+
+(def config
+  (:aliyun-oss-ram-config
+   (read-string (slurp "config/config.clj"))))
+
+(def REGION_CN_HANGZHOU (:region config))
+(def STS_API_VERSION "2015-04-01")
+(def accessKeyId (:accessKeyId config))
+(def accessKeySecret (:accessKeySecret config))
+(def roleArn (:rolearn config))
+(def roleSessionName (:roleSessionName config))
+(def protocolType ProtocolType/HTTPS)
+(def policy (:policy config))
+
+;;(assumeRole accessKeyId accessKeySecret roleArn roleSessionName policy protocolType)
+;;=> #object[com.aliyuncs.sts.model.v20150401.AssumeRoleResponse 0x51902f96 "com.aliyuncs.sts.model.v20150401.AssumeRoleResponse@51902f96"]
+(defn assumeRole
+  [^String accessKeyId ^String accessKeySecret
+   ^String roleArn ^String roleSessionName ^String policy
+   ^ProtocolType protocolType]
+  (let [profile (DefaultProfile/getProfile REGION_CN_HANGZHOU accessKeyId accessKeySecret)
+        client (DefaultAcsClient. profile)
+        request (AssumeRoleRequest.)
+        _ (.setVersion request STS_API_VERSION)
+        _ (.setMethod request MethodType/POST)
+        _ (.setProtocol request protocolType)
+        _ (.setRoleArn request roleArn)
+        _ (.setRoleSessionName request roleSessionName)
+        _ (.setPolicy request policy)]
+    (.getAcsResponse client request)
+    )
+  )
+
+
