9ba15d679cc49717448d68402cb0ed575387dd54
chanshunli <chanshunli@gmail.com>
2020-01-13 10:26:05 +0800

    所有库更新到最新的版本

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject aliyun-oss-clj "0.1.2-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject aliyun-oss-clj "0.1.3"
+  :description "阿里云OSS的Clojure SDK封装"
+  :url "https://github.com/chanshunli/aliyun-oss-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.aliyun.oss/aliyun-sdk-oss "2.6.0"]
-                 [com.aliyun/aliyun-java-sdk-sts "2.1.6"]
-                 [com.aliyun/aliyun-java-sdk-core "2.1.7"]
-                 [cheshire "5.7.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.aliyun.oss/aliyun-sdk-oss "3.8.1"]
+                 [com.aliyun/aliyun-java-sdk-sts "3.0.1"]
+                 [com.aliyun/aliyun-java-sdk-core "4.4.9"]
+                 [cheshire "5.9.0"]])
